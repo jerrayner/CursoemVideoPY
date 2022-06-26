a64d85b65979efbff8e90b293a5f85186eccfeef
Jerrayne Rodrigues <jerrayner@gmail.com>
2022-06-26 20:20:09 -0300

    enviando primeiros exercicios basicos

---
+++ aula006.py
@@ -0,0 +1,5 @@
+num1 = int(input('Digite um número: ')) # sempre que for fazer alguma operação com números, coloca-se o tipo primitivo do número antes do input, assim como para valores booleanos, números com pontos flutuantes. O valor recebido pelo input sempre será uma string.
+num2 = int(input('Digite outro: '))
+som = int(num1 + num2)
+print(f'A soma entre {num1} e {num2} vale {som}') # para facilitar a digitação e retornar as variaveis em um print no console, apenas coloca 'f' antes do que será exibido no input e chama as variaveis dentro de chaves no print.
+
---
+++ ex004.py
@@ -0,0 +1,9 @@
+something = input('Digite algo: ')
+print('O tipo primitivo desse valor é', type(something))
+print('Só tem espaços? ', something.isspace())
+print('É um numero? ', something.isnumeric())
+print('É alfabético? ', something.isalpha())
+print('Está escrito em letras maisculas? ', something.isupper())
+print('Está escrito em letras minusculas?', something.islower())
+print('Esta capitalizado?', something.istitle())
+
